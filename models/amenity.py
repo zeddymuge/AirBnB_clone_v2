2d3a7ccbc1825155ccc1b886a0a886c430a7e455
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-01-06 13:29:00 +0300

    done

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
-    name = ""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+
+
+class Amenity(BaseModel, Base):
+    """Class Amenity Containing the most basic info """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   backref='places')
