2c614bcd2ec4a5b4e81f5cbe1a45e37272361d22
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-01-06 13:15:00 +0300

    done

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class definition in the next line"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances"""
+        from models import storage
+        from models.city import City
+        new_list = []
+        for key, obj_city in storage.all(City).items():
+            if obj_city.state_id == self.id:
+                new_list.append(obj_city)
+        return new_list
